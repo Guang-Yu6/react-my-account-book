646d5669483835c64c6b93086784e97a3decbc1c
Guang-Yu6 <everyting-day@foxmail.com>
2021-07-09 14:26:31 +0800

    初始化完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: #61dafb;
+  background: green;
+`
+const Div = styled.div`
+  border: 5px solid pink;
+  min-width: 300px;
+  min-height: 300px;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <Button>不会吧不会吧</Button>
+      <h3>123</h3>
+    </Div>
   );
 }
 
