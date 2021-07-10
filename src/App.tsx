f192281b96a9268a49e39316707cbb3bb7543cc5
Guang-Yu6 <everyting-day@foxmail.com>
2021-07-10 13:37:16 +0800

    创建 views 文件夹

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Layout from './Components/Layout';
+
+import Tags from 'views/Tags';
+import Money from 'views/Money';
+import Statistics from 'views/Statistics';
+import NoMatch from 'views/NoMatch';
 import {
   HashRouter as Router,
   Switch,
   Route,
   Redirect
 } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -24,39 +29,12 @@ function App() {
             </Route>
 
             <Route path='*'>
-              <A/>
+              <NoMatch/>
             </Route>
-
             <Redirect exact from="/" to="/money"/>
           </Switch>
     </Router>
   );
-}
-
-function A() {
-  return <h2>页面不存在</h2>;
-}
-
-function Tags() {
-  return (
-      <Layout>
-        <h2>标签页面</h2>
-      </Layout>
-  )
-}
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页面</h2>
-    </Layout>
-  )
-}
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>
-    </Layout>
-  )
 }
 
 export default App;
