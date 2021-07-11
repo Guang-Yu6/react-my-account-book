3b953971e1092bf322061f8c08c0699f6bc80204
Guang-Yu6 <everyting-day@foxmail.com>
2021-07-11 10:07:33 +0800

    修复点击导航栏区域的问题

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -14,40 +14,50 @@ const NavCss = styled.nav`
     > li {
       width: 33.33333%;
       text-align: center;
+      > a {
       padding: 4px 0;
       display: flex;
       flex-direction: column;  // 上下部剧
       align-items: center;  // svg 图标居中对齐文字
-      .icon {
+        .icon {
         width: 24px;
         height: 24px;
       }
+      }
+      
   }
     }
-`
+`;
 
 const Nav = () => {
   return (
     <NavCss>
       <ul>
         <li>
-         <Icon name='tags'/>
-          <Link to="tags">标签</Link>
+          <Link to="tags">
+            <Icon name='tags'/>
+            标签
+          </Link>
         </li>
 
         <li>
-          <Link to="/money">记账</Link>
-          <Icon name="money"/>
+          <Link to="/money">
+            <Icon name="money"/>
+            记账
+          </Link>
+
         </li>
 
         <li>
-          <Icon name='chart'/>
-          <Link to="/Statistics">统计</Link>
+          <Link to="/Statistics">
+            <Icon name='chart'/>
+            统计
+          </Link>
         </li>
       </ul>
     </NavCss>
-  )
-}
+  );
+};
 
 
-export default Nav
+export default Nav;
